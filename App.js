96866eb933a7c6c4c1d7df716a087468a4438d56
ngactive99 <ngactive99@gmail.com>
2022-02-10 17:30:47 +0530

    list view using map and scroll view

--- App.js
+++ App.js
@@ -1,99 +1,60 @@
 import React, {useState} from 'react';
 // import type {Node} from 'react';
-import {
-  Button,
-  StyleSheet,
-  Text,
-  View,
-  Linking,
-  ImageBackground,
-  Image,
-  TouchableHighlight,
-  Alert,
-} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 const App = () => {
-  const [name, setName] = useState('style training!');
-
-  const onClickHandler = () => {
-    setName('React native training');
-  };
-
+  const [Items, setItems] = useState([
+    {key: 1, item: 'Item 1'},
+    {key: 2, item: 'Item 2'},
+    {key: 3, item: 'Item 3'},
+    {key: 4, item: 'Item 4'},
+    {key: 5, item: 'Item 5'},
+    {key: 6, item: 'Item 6'},
+    {key: 7, item: 'Item 7'},
+    {key: 8, item: 'Item 8'},
+    {key: 9, item: 'Item 9'},
+    {key: 10, item: 'Item 10'},
+    {key: 11, item: 'Item 11'},
+  ]);
   return (
     <View style={styles.body}>
-      <View style={styles.view1}>
-        <Text style={styles.text}>1</Text>
-      </View>
-      <View style={styles.view2}>
-        <Text style={styles.text}>2</Text>
-      </View>
-      <View style={styles.view3}>
-        <Text style={styles.text}>3</Text>
-      </View>
-
-      {/* <Button
-        title="greet me"
-        onPress={() => {
-          Linking.openURL('https://www.bbc.com');
-        }}></Button> */}
-      {/* <TouchableHighlight onPress={()=> {Alert.alert("the touchable button has been pressed")}}
-        activeOpacity={0.5}
-        underlayColor={"#076B0E"}>
-          <Text>Click me </Text>
-        </TouchableHighlight> */}
+      <ScrollView>
+      {Items.map(i => {
+        return (
+          <View style={styles.item}>
+            <Text style={styles.text}>{i.item}</Text>
+          </View>
+        );
+      })}
+      </ScrollView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   body: {
-    flex:1,
-    flexDirection:"column",
-    backgroundColor: '#C73AF8',
-    alignItems: 'stretch',
-    justifyContent: 'center',
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#FFFFFF',
+    // alignItems: 'stretch',
+    // justifyContent: 'center',
     // borderBottomColor: "#FF1106",
     // borderWidth:10,
     // borderColor:'#3BE8F2',
     // borderRadius:10,
   },
+  item: {
+    margin:10,
+    backgroundColor: '#3A52F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   text: {
     color: '#000000',
-    fontSize: 20,
+    fontSize: 45,
     fontStyle: 'italic',
     margin: 10,
     textTransform: 'uppercase',
   },
-  bgimage: {
-    flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center',
-  },
-  image: {
-    width: 150,
-    height: 120,
-  },
-  button: {
-    width: 150,
-    height: 60,
-  },
-  view1: {
-    flex:1,
-    backgroundColor:"#00ffff",
-    alignItems:'center',
-    justifyContent:'center'
-  },
-  view2: {
-    flex:1,
-    backgroundColor:"#F8763A",
-    alignItems:'center',
-    justifyContent:'center'
-  },
-  view3: {
-    flex:1,
-    backgroundColor:"#B3F83A",
-    alignItems:'center',
-    justifyContent:'center'
-  }
 });
 
 export default App;
