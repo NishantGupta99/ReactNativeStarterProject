cfdfe5b49fd18e6c6cb2a8902d17dc3807307a31
ngactive99 <ngactive99@gmail.com>
2022-02-11 10:05:58 +0530

    compleated flat list with refresh control

--- components/flatlist.js
+++ components/flatlist.js
@@ -10,24 +10,35 @@ import {
 
 const Flat= () => {
   const [Items, setItems] = useState([
-    {key: '1', name: 'Item 1'},
-    {key: '2', name: 'Item 2'},
-    {key: '3', name: 'Item 3'},
-    {key: '4', name: 'Item 4'},
-    {key: '5', name: 'Item 5'},
-    {key: '6', name: 'Item 6'},
-    {key: '7', name: 'Item 7'},
-    {key: '8', name: 'Item 8'},
-    {key: '9', name: 'Item 9'},
-    {key: '10', name: 'Item 10'},
-    {key: '11', name: 'Item 11'},
+    { name: 'Item 1'},
+    { name: 'Item 2'},
+    { name: 'Item 3'},
+    { name: 'Item 4'},
+    { name: 'Item 5'},
+    { name: 'Item 6'},
+    { name: 'Item 7'},
+    { name: 'Item 8'},
+    { name: 'Item 9'},
+    { name: 'Item 10'},
+    { name: 'Item 11'},
   ]);
+  const [Refreshing, setRefreshing] = useState(false)
+  const onRefresh = () => {
+    setRefreshing(true);
+    setItems([...Items, {name : 'Item 404'}]);
+    setRefreshing(false)
+  }
   return (
     <FlatList
+    //numColumns={2}
+        // horizontal
+        // inverted
+        keyExtractor={(item,index)=> index.toString()}
+        refreshControl={<RefreshControl refreshing={Refreshing} onRefresh={onRefresh} colors={['#F08914']} />}
       data={Items}
       renderItem={({item}) => (
         <View style={styles.item} >
-          <Text style={styles.text}>{item.name}</Text>
+          <Text style={styles.text}>{item.name} </Text>
         </View>
       )}
     />
